55267937078321a827ee025e2ef4299955b26d18
Minji Kim <mjkim@hcil.snu.ac.kr>
2018-07-06 18:24:53 +0900

    GameSystem fixed

--- VuforiaPractice/Assets/GameSystem.cs
+++ VuforiaPractice/Assets/GameSystem.cs
@@ -5,6 +5,31 @@ using UnityEngine;
 public class GameSystem : MonoBehaviour {
     private GameSystem m_Instance;
     public GameSystem Instance { get { return m_Instance; } }
+    private GameObject m_currobj;       // object which is currently selected by the user
+    private List<Vector3> m_vertices;   // initial vertices of the object (before selected),
+                                        // redundant values removed
+    private List<Vector3> s_vertices;   // vertices that are selected by the user
+    private int m_mode; // 0: divide meshes, 1: select physics, 2: select material
+    private List<GameObject> objects;
+    public ObjectBehavior m_script;
+
+    void SetVertices(Vector3[] v)
+    {
+        for (int i = 0; i < v.Length; i++)
+        {
+            m_vertices[i] = v[i];
+        }
+    }
+
+    void ClearVertices()
+    {
+        m_vertices.Clear();
+    }
+
+    void ScriptManager()
+    {
+
+    }
 
     void Awake()
     {
@@ -17,14 +42,28 @@ public class GameSystem : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        m_Instance = this;
+        m_vertices = new List<Vector3>();
+        m_mode = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if button clicked
+        // select vertices (how do we know that vertices have chosen?)
+        // if the object is selected, then the object script set the vertices of the game system instance
+        // and also show the vertices on the screen
+        // when clicked and dragged, vertices are selected if close enough
+
+        // call the object's divide mesh function in here
+
+        // if button clicked, make the user select the part
+        // then gives the physics to that specific part
+
+    }
 
     void OnGui()
     {
